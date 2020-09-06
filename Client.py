96fc0bf12bee17eed95f6dc9f70a3dadd6297c62
slavujevic96 <jotzilla96@gmail.com>
2020-09-06 14:18:29 -0400

    Client-Server communication, frame transmission through TCP/IP socket

---
+++ Client.py
@@ -0,0 +1,39 @@
+import cv2
+import io
+import socket
+import struct
+import time
+import pickle
+import zlib
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+port = 21000
+host = socket.gethostname() # Get local machine name
+
+client_socket.connect((host, port) )
+connection = client_socket.makefile('wb')
+
+data = b""
+payload_size = struct.calcsize(">L")
+while True:
+    while len(data) < payload_size:
+        data += client_socket.recv(4096)
+
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack(">L", packed_msg_size)[0]
+
+    while len(data) < msg_size:
+        data += client_socket.recv(4096)
+        
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+
+    frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+    cv2.imshow('ImageWindow',frame)
+    cv2.waitKey(1)
+
+client.close()
+cv2.destroyAllWindows()
--- Server.py
+++ Server.py
@@ -10,6 +10,12 @@ import time
 
 #sys lib for checking a platform (OS)
 from sys import platform
+
+import struct
+import pickle
+
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
 #face detection classifier
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #WebCam handler
@@ -17,6 +23,8 @@ if platform == "linux" or platform == "linux2":
     cap = cv2.VideoCapture(0)
 elif platform == "win32":
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(3, 320)
+cap.set(4, 240)
 
 #socket server's IP address & port 
 port = 21000
@@ -26,30 +34,35 @@ host = socket.gethostname() # Get local machine name
 clients = set()
 clients_lock = threading.Lock()
 
-global serversock
 serversock = socket.socket()
 serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serversock.bind((host,port))
 serversock.listen(3)
 th = []
 
+ClientConnection = True
+
 def listener(client, address):
-    global img
+    global data
+    global sndMsg
     print ("\nAccepted connection from: ", address,'\n')
     with clients_lock:
         clients.add(client)
-    try:    
-        while True:
-            data = client.recv(1024)
-            if int(data) == 0:
-                msg = "HI :)"
-                print('\nSENDING: ', str(msg),'\n')
-                #char = str(int(catch))
-                client.send(bytes(msg,encoding='utf8'))
-    except ValueError:
-        print("\nClient Disconnected\n")
+
+    while ClientConnection:
+        if sndMsg == True:
+            try:
+                client.sendall(struct.pack(">L", len(data) ) + data)
+                sndMsg = False
+            except BrokenPipeError:
+                break
+    
+    print("\nBroken connection from: ", address, "\n")
+    clients.remove(client)
 
 def clientReceivement():
+    global sndMsg
+    sndMsg = False
 
     print ("\nWaiting for new clients...\n")
     while True:
@@ -64,23 +77,52 @@ def clientReceivement():
 
 th.append(Thread(target=clientReceivement) )
 th[-1].start()
-while True:
-    global img
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for(x, y, w, h) in faces:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (0, 0, 255), 2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
 
-    cv2.imshow('img', img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
+
+incr = 0
+limit = 40000
+
+while True:
+    try:        
+        incr = incr + 1
+
+        if(incr < limit):
+            continue
+        elif(incr == limit):
+            incr = 0
+
+        ret, frame = cap.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for(x, y, w, h) in faces:
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+
+        # cv2.imshow('img', frame)
+        # k = cv2.waitKey(30) & 0xff
+        # if k == 27:
+        #     break
+
+        # if(len(faces) == 0):
+        #     continue
+
+        sndMsg = True
+        result, frame = cv2.imencode('.jpg', frame, encode_param)
+        data = pickle.dumps(frame, 0)
+
+    except KeyboardInterrupt:
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
+for client in clients:
+    client.shutdown(socket.SHUT_RDWR)
+    client.close()
+
+ClientConnection = False
 
 try:
     serversock.shutdown(socket.SHUT_RDWR)
@@ -88,9 +130,9 @@ try:
 except OSError:
     serversock.close()
 
-for client in clients:
-    client.shutdown(socket.SHUT_RDWR)
-    client.close()
+
+if(clients_lock.locked() == True):
+    clients_lock.release()
 
 for thd in th:
     thd.join()
